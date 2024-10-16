0590f24af95722285ab3b945dfe47ba54cb106e2
D.Deepak Bajpai <133989782+Deepak-cell311@users.noreply.github.com>
2024-10-16 14:02:41 +0530

    Update Payment.jsx

--- fund-raising/src/Component/Payment.jsx
+++ fund-raising/src/Component/Payment.jsx
@@ -26,7 +26,7 @@ const Payment = () => {
         };
 
         try {
-            const response = await fetch('http://localhost:5000/api/payments', {
+            const response = await fetch('https://fund-raising-psi.vercel.app/payments', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
