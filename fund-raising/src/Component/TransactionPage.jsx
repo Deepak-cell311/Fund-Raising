4526d6417baa971691fdcf5745b1d79b4c7c8bb0
D.Deepak Bajpai <133989782+Deepak-cell311@users.noreply.github.com>
2024-10-16 14:12:14 +0530

    Update TransactionPage.jsx

--- fund-raising/src/Component/TransactionPage.jsx
+++ fund-raising/src/Component/TransactionPage.jsx
@@ -9,7 +9,7 @@ const TransactionPage = () => {
     useEffect(() => {
         const fetchPayments = async () => {
             try {
-                const response = await fetch('http://localhost:5000/api/payments');
+                const response = await fetch('https://fund-raising-psi.vercel.app/payments');
                 if (!response.ok) {
                     throw new Error('Failed to fetch payments');
                 }
