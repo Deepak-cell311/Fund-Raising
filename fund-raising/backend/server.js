757a6382d34b1b646d9eace48454d01c7e2500e5
D.Deepak Bajpai <133989782+Deepak-cell311@users.noreply.github.com>
2024-10-16 21:47:08 +0530

    Update server.js

--- fund-raising/backend/server.js
+++ fund-raising/backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 const corsOption = {
   // origin: "https://fund-raising-psi.vercel.app/",
-  origin: "*",
+  origin: "https://fund-raising-psi.vercel.app",
   methods: "GET, POST, PUT, DELETE, HEAD, PATCH",
   allowedHeaders: ['Content-Type', 'Authorization']
   credentials: true,
