a231a49a66d0c3a706cad4d7babdaddc5cb35f13
D.Deepak Bajpai <133989782+Deepak-cell311@users.noreply.github.com>
2024-10-16 21:39:45 +0530

    Update DonationPage.jsx

--- fund-raising/src/Component/DonationPage.jsx
+++ fund-raising/src/Component/DonationPage.jsx
@@ -27,7 +27,7 @@ const DonationPage = () => {
 
         try {
             console.log('Sending donation data:', donationData);
-            const response = await fetch('https://fund-raising-5.onrender.com/api/donations', {
+            const response = await fetch('https://fund-raising-r319.vercel.app/api/donations', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
