d2ba64a95c9903b899bc58bcd46465789824b62a
D.Deepak Bajpai <133989782+Deepak-cell311@users.noreply.github.com>
2024-10-16 23:43:05 +0530

    Update server.js

--- fund-raising/backend/server.js
+++ fund-raising/backend/server.js
@@ -68,7 +68,7 @@ app.use((err, req, res, next) => {
 // });
 
 // Start server
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-module.exports = app;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+// module.exports = app;
